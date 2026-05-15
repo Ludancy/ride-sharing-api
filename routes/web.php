5b11c3e8494801a10403649d1d69d746337a157c
Luis Cortez <danielcorteznew8@gmail.com>
2026-05-14 22:00:45 -0400

    logs routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/logs', function (\Illuminate\Http\Request $request) {
+    if ($request->query('key') !== 'verlogs2026') {
+        abort(403, 'Unauthorized access');
+    }
+
+    $logPath = storage_path('logs/laravel.log');
+
+    if (!\Illuminate\Support\Facades\File::exists($logPath)) {
+        return response('Log file not found.', 404);
+    }
+
+    return response(\Illuminate\Support\Facades\File::get($logPath), 200, [
+        'Content-Type' => 'text/plain'
+    ]);
+});
